aa47b8a178ff4c36f4cc59cf5ff85d0229006b1f
Sohel Shaikh <sohelshaikh97@gmail.com>
2019-01-15 15:10:07 +0530

    Added sem 5

--- js/main.js
+++ js/main.js
@@ -41,6 +41,30 @@ function calculateFour() {
 
 }
 
+function calculateFive() {
+  var x = document.getElementById("gradeForm");
+  var gp = 0;
+  var tgp = 250; // total grade points that can be achieved
+
+  // gp achieved * credits
+  gp = gp + parseInt(x.elements[0].value);
+  gp = gp + (parseInt(x.elements[1].value) * 2);
+  gp = gp + (parseInt(x.elements[2].value) * 3);
+  gp = gp + (parseInt(x.elements[3].value) * 4);
+  gp = gp + ((parseInt(x.elements[4].value) + parseInt(x.elements[5].value) + parseInt(x.elements[6].value)) * 5)
+
+
+  var spi = (gp / tgp) * 10;
+
+  // display result
+  if (spi) {
+    document.getElementById("marks").innerHTML = spi;
+  } else {
+    document.getElementById("marks").innerHTML = "invalidInput :(";
+  }
+
+}
+
 function refresh() {
   window.location.reload(false);
 }
